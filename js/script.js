9d53631a192de0f39c87271cf5fe96a8339716c1
Robson Vinicius <robsondgn@outlook.com>
2020-08-17 16:49:15 -0300

    Criando hover do button comprar e troca de cor do button ao clicar nos radios

--- js/script.js
+++ js/script.js
@@ -3,4 +3,5 @@ $(".product-colors span").click(function(){
   $(this).addClass("active");
   $("body").css("background", $(this).attr("data-color")); // Troca de cor de fundo
   $(".product-price").css("color", $(this).attr("data-color")); // Troca de cor do preço
+  $(".product-button").css("color",$(this).attr("data-color")); // Troca de cor do botão
 })
