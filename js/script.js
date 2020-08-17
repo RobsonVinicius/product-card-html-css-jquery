fe8f53e6f6c40ed12604704ae33b28775dac5463
Robson Vinicius <robsondgn@outlook.com>
2020-08-17 16:57:09 -0300

    Animação e script da troca de imagens através dos radios

--- js/script.js
+++ js/script.js
@@ -4,4 +4,5 @@ $(".product-colors span").click(function(){
   $("body").css("background", $(this).attr("data-color")); // Troca de cor de fundo
   $(".product-price").css("color", $(this).attr("data-color")); // Troca de cor do preço
   $(".product-button").css("color",$(this).attr("data-color")); // Troca de cor do botão
+  $(".product-pic").css("background-image",$(this).attr("data-pic")); // Troca de imagens dos tênis (Difentes cores)
 })
