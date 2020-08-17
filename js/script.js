224a773916fe37ddcc137bac5342614ebfd9985c
Robson Vinicius <robsondgn@outlook.com>
2020-08-17 16:42:47 -0300

    Criando a troca de cor do preço ao clicar nos radios

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 $(".product-colors span").click(function(){
   $(".product-colors span").removeClass("active");
   $(this).addClass("active");
-  $("body").css("background", $(this).attr("data-color"));
+  $("body").css("background", $(this).attr("data-color")); // Troca de cor de fundo
+  $(".product-price").css("color", $(this).attr("data-color")); // Troca de cor do preço
 })
